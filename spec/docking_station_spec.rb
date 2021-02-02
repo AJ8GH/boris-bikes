7ce30247d9b34013626ce3bfc284efe7ce3314a4
Adam Jonas <adamjonas25@gmail.com>
2021-02-02 16:01:20 +0000

    define Bike#working?

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,7 +7,6 @@ describe DockingStation do
     expect { DockingStation.new }.to_not raise_error
   end
 
-
   describe '#release_bike' do
     it 'responds to release_bike' do
       expect(docking_station).to respond_to(:release_bike)
