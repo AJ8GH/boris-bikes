249a4f6723ae5f7a244925aad389b53e1520701b
Adam Jonas <adamjonas25@gmail.com>
2021-02-02 15:52:12 +0000

    define release_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,16 @@
 require 'docking_station'
 
 describe DockingStation do
-  describe '#intialize' do
-    it 'creates a new DockingStation' do
-      expect { DockingStation.new }.to_not raise_error
+  subject (:docking_station) { DockingStation.new }
+
+  it 'creates a new DockingStation' do
+    expect { DockingStation.new }.to_not raise_error
+  end
+
+
+  describe '#release_bike' do
+    it 'responds to release_bike' do
+      expect(docking_station).to respond_to(:release_bike)
     end
   end
 end
